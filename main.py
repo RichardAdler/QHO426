b1edf5ea0f408938c5d8149eacea4e339a4c3e62
Richard Adler <84353118+RichardAdler@users.noreply.github.com>
2022-03-12 11:23:42 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-def listen():
-    sound = input("What sound did I hear?\n")
-    return sound
+def identify():
+    see = input("What lies before us?")
+    if see == "a large boulder":
+        print("It's time to run!")
 
-def answer(a):
-    if a == "rumble":
-        print("That was a loud rumble!")
-    elif a == "beep":
-        print("It was R2D2.")
     else:
-        print("It is quite.")
+        print("We will be fine.")
 
 
-
-def run():
-    sound = listen()
-    answer(sound)
-
-run()
+identify()
